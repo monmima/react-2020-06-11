ea71c73f5b3f50c67bb2a1fa4e433426a5c8a556
monmima <monmima@protonmail.com>
2020-06-11 17:54:29 -0400

    created HTML examples

---
+++ src/Banner.js
@@ -0,0 +1,22 @@
+/**
+ * The following line would be necessary with "npx create-react-dom".
+ */
+// import React from 'react';
+
+class Banner extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron bandeau flex justify-content-center">
+                <img src="http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-25/256/Baldur-s-Gate-3-icon.png" alt="Logo de Baldur's Gate" />
+                <img src="http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-25/256/Baldur-s-Gate-2-Shadows-of-Amn-1-icon.png" alt="Logo de Baldur's Gate 2" />
+                <img src="http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-25/256/Icewind-Dale-3-icon.png" alt="Logo d'Icewind Dale" />
+                <img src="http://icons.iconarchive.com/icons/3xhumed/mega-games-pack-25/256/Baldur-s-Gate-2-Throne-of-Bhaal-1-icon.png" alt="Logo de Throne of Bhaal" />
+            </div>
+        );
+    }
+}
+
+/**
+ * The following line would be necessary with "npx create-react-dom".
+ */
+// export default Banner;
